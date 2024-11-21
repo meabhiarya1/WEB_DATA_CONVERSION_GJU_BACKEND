0a94695103da03c7aef933e1c5beaa0547cccd9b
unknown <abhishekmishra992016@gmail.com>
2024-11-21 09:24:21 +0530

    remove comment on database

--- utils/database.js
+++ utils/database.js
@@ -1,20 +1,6 @@
-// const Sequelize = require("sequelize");
-
-// const sequelize = new Sequelize("omrusermanagement", "root", "12345678", {
-//   dialect: "mysql",
-//   host: "localhost",
-// });
-
-// module.exports = sequelize;
-
-// const Sequelize = require("sequelize");
-
-// const sequelize = new Sequelize("omrscannerduplex", "root", "root", {
-//   dialect: "mysql",
-//   host: "localhost",
-// });
 
 const Sequelize = require("sequelize");
+
 
 const sequelize = new Sequelize("gjuwebdataconversion", "root", "root", {
   dialect: "mysql",
